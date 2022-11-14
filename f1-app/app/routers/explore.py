80da6bd8ea2b7770782ce10e0f736c891de7a44d
Seth Billiau <sethbilliau@gmail.com>
2022-11-13 22:02:36 -0500

    Flake8 the python code
    update style for codebase. No functionality is changed.

--- f1-app/app/routers/explore.py
+++ f1-app/app/routers/explore.py
@@ -2,11 +2,12 @@ from fastapi import Request, APIRouter
 from fastapi.templating import Jinja2Templates
 
 
-# Mount templates 
+# Mount templates
 templates = Jinja2Templates(directory="templates")
 
 # Create API Router
 router = APIRouter()
+
 
 @router.get("/explore")
 async def root(request: Request):
