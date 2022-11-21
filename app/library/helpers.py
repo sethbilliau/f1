a10f1b45eac279cafb362d8e3fb29626fadbe91f
Seth Billiau <sethbilliau@gmail.com>
2022-11-20 20:15:57 -0500

    merge from dev

--- app/library/helpers.py
+++ app/library/helpers.py
@@ -3,7 +3,8 @@
 import pandas as pd
 import random
 from typing import Optional
-
+import boto3
+import json
 
 def get_random_starting_ending_drivers(seed: Optional[int] = None):
     if seed:
@@ -13,12 +14,31 @@ def get_random_starting_ending_drivers(seed: Optional[int] = None):
     row = gamesdf.iloc[row_index]
     return {'driver1': row['driver1'], 'driver2': row['driver2']}
 
+
+def get_starting_ending_drivers_from_s3(region, id, secretkey, bucket, key):
+    s3 = boto3.resource(
+        service_name='s3',
+        region_name=region,
+        aws_access_key_id=id,
+        aws_secret_access_key=secretkey
+    )
+    obj = s3.Bucket(bucket).Object(key).get()
+    file_content = obj['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    return {
+        'driver1': json_content[0]['driver1'],
+        'driver2': json_content[0]['driver2']
+    }
+
+
 def getFullNamesList():
     namesdf = pd.read_csv('app/data/drivers.csv')
     namesListFull = list(namesdf.iloc[:,0])
     return namesListFull
 
+
 namesListFull = getFullNamesList()
+
 
 def add_node(newNode: dict, names_: list, nodes_: list,
              nodesSet_: set, _driverList: list, namesList: list = namesListFull):
